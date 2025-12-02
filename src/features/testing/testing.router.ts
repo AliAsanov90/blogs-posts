b5f27872b03449730e5fc8d621406d0980643c2f
Ali Asanov <aliasanov07@gmail.com>
2025-12-03 00:23:10 +0300

    delete users in delete all data endpoint

--- src/features/testing/testing.router.ts
+++ src/features/testing/testing.router.ts
@@ -1,12 +1,17 @@
 import { Request, Response, Router } from 'express'
 import { ALL_DATA } from '../../common/constants/routes'
 import { HttpStatus } from '../../common/types/http-statuses.types'
-import { blogsCollection, postsCollection } from '../../db/mongo.db'
+import {
+  blogsCollection,
+  postsCollection,
+  usersCollection,
+} from '../../db/mongo.db'
 
 const deleteAllData = async (req: Request, res: Response) => {
   await Promise.all([
     blogsCollection.deleteMany(),
     postsCollection.deleteMany(),
+    usersCollection.deleteMany(),
   ])
   res.sendStatus(HttpStatus.NoContent)
 }
